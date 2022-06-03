d1a564fc84ba1ed4960e05bc71dba4695878863f
BobMali <BobMali@users.noreply.github.com>
2022-06-03 14:12:19 +0200

    fix version screw up

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,5 +23,5 @@ $EM_CONF[$_EXTKEY] = [
     'author'           => 'Susanne Moog, Steffen Frese, Tobias Kretschmann',
     'author_email'     => 's.moog@neusta.de, s.frese@neusta.de, t.kretschmann@neusta.de',
     'author_company'   => 'Neusta GmbH',
-    'version'          => '1.3.0',
+    'version'          => '2.1.0',
 ];
