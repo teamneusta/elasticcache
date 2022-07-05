78a7754e4f75535edd30969cb44e23ba26663c16
Malek Olabi <m.olabi@neusta.de>
2022-07-05 13:00:20 +0200

    [TASK] update to accommodate Elasticsearch v7

--- Classes/Cache/Backend/ElasticsearchBackend.php
+++ Classes/Cache/Backend/ElasticsearchBackend.php
@@ -1,5 +1,5 @@
 <?php
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace TeamNeusta\Elasticcache\Cache\Backend;
 
@@ -7,12 +7,10 @@ use Elastica\Client;
 use Elastica\Document;
 use Elastica\Exception\NotFoundException;
 use Elastica\Index;
-use Elastica\Query\Match;
 use Elastica\Query\MatchAll;
+use Elastica\Query\MatchQuery;
 use Elastica\Query\Range;
-use Elastica\Response;
 use Elastica\Search;
-use Elastica\Type;
 use TYPO3\CMS\Core\Cache\Backend\AbstractBackend;
 use TYPO3\CMS\Core\Cache\Backend\TaggableBackendInterface;
 use TYPO3\CMS\Core\Cache\Backend\TransientBackendInterface;
@@ -22,30 +20,17 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
 class ElasticsearchBackend extends AbstractBackend implements TaggableBackendInterface, TransientBackendInterface
 {
     protected string $hostname = 'localhost';
-    /**
-     * Document Type Name
-     */
-    protected string $typeName = 'cacheEntry';
     protected Client $elastica;
     protected Index $index;
-    protected Type $type;
     protected int $port = 9200;
     protected string $path = '/';
     protected string $transport = 'http';
     protected string $indexName = 't3cache';
     /**
      * Path to index configuration yaml
-     * Example: EXT:myext/Configuration/Elastic/indexConfiguration.yaml
+     * Example: EXT:myext/Configuration/Elastic/indexConfiguration.yaml.
      */
     protected string $indexConfiguration = '';
-
-    /**
-     * @return Type
-     */
-    public function getType(): Type
-    {
-        return $this->type;
-    }
 
     /**
      * @return Index
@@ -88,14 +73,6 @@ class ElasticsearchBackend extends AbstractBackend implements TaggableBackendInt
     }
 
     /**
-     * @param string $typeName
-     */
-    public function setTypeName(string $typeName): void
-    {
-        $this->typeName = $typeName;
-    }
-
-    /**
      * @param int $port
      */
     public function setPort(int $port): void
@@ -127,10 +104,9 @@ class ElasticsearchBackend extends AbstractBackend implements TaggableBackendInt
                 'port'      => $this->port,
                 'path'      => $this->path,
                 'transport' => $this->transport,
-            ]
+            ],
         );
         $this->index = $this->elastica->getIndex($this->indexName);
-        $this->type = $this->index->getType($this->typeName);
         if (!$this->index->exists()) {
             $this->createIndex();
             // add wait time until index was created before writing
@@ -141,11 +117,11 @@ class ElasticsearchBackend extends AbstractBackend implements TaggableBackendInt
     /**
      * Saves data in the cache.
      *
-     * @param string $entryIdentifier An identifier for this specific cache entry
-     * @param string $data            The data to be stored
-     * @param array  $tags            Tags to associate with this cache entry. If the backend does not support tags, this option
-     *                                can be ignored.
-     * @param int    $lifetime        Lifetime of this cache entry in seconds. "0" means unlimited lifetime.
+     * @param string   $entryIdentifier An identifier for this specific cache entry
+     * @param string   $data            The data to be stored
+     * @param string[] $tags            Tags to associate with this cache entry. If the backend does not support tags, this option
+     *                                  can be ignored.
+     * @param int      $lifetime        Lifetime of this cache entry in seconds. "0" means unlimited lifetime.
      */
     public function set($entryIdentifier, $data, array $tags = [], $lifetime = null): void
     {
@@ -158,9 +134,9 @@ class ElasticsearchBackend extends AbstractBackend implements TaggableBackendInt
             [
                 'content'   => $data,
                 '_tags'     => $tags,
-                '_lifetime' => $lifetime
+                '_lifetime' => $lifetime,
             ],
-            $this->typeName
+            $this->index ?? '',
         );
         $this->index->addDocuments([$document]);
     }
@@ -179,18 +155,6 @@ class ElasticsearchBackend extends AbstractBackend implements TaggableBackendInt
         } catch (NotFoundException $e) {
             return false;
         }
-    }
-
-    private function getEntryWithLifeTimeCheck(string $entryIdentifier)
-    {
-        $entry = $this->type->getDocument($entryIdentifier);
-        $data = $entry->getData();
-        $content = false;
-        if ($data['_lifetime'] === 0 || $data['_lifetime'] > time()) {
-            $content = $data['content'];
-        }
-
-        return $content;
     }
 
     /**
@@ -224,7 +188,7 @@ class ElasticsearchBackend extends AbstractBackend implements TaggableBackendInt
             return false;
         }
 
-        $this->type->deleteById($entryIdentifier);
+        $this->index->deleteById($entryIdentifier);
 
         return true;
     }
@@ -235,21 +199,21 @@ class ElasticsearchBackend extends AbstractBackend implements TaggableBackendInt
     public function flush(): void
     {
         $matchAll = new MatchAll();
-        $this->type->deleteByQuery($matchAll);
+        $this->index->deleteByQuery($matchAll);
     }
 
     /**
      * Does garbage collection
-     * Removes expired entries from the cache
+     * Removes expired entries from the cache.
      */
     public function collectGarbage(): void
     {
         $range = new Range();
         $range->addField('_lifetime', [
             'from' => 1,
-            'to'   => time()
+            'to'   => time(),
         ]);
-        $this->type->deleteByQuery($range);
+        $this->index->deleteByQuery($range);
     }
 
     /**
@@ -259,32 +223,37 @@ class ElasticsearchBackend extends AbstractBackend implements TaggableBackendInt
      */
     public function flushByTag($tag): void
     {
-        $query = new Match('_tags', $tag);
-        $this->type->deleteByQuery($query);
+        $query = new MatchQuery('_tags', $tag);
+        $this->index->deleteByQuery($query);
     }
 
     /**
      * Finds and returns all cache entry identifiers which are tagged by the
      * specified tag
      * elasticsearch scan and scroll is used to make sure all matching entries are returned independent from the limit
-     * of results per page
+     * of results per page.
      *
      * @param string $tag The tag to search for
      *
-     * @return array An array with identifiers of all matching entries. An empty array if no entries matched
+     * @return string[] An array with identifiers of all matching entries. An empty array if no entries matched
+     *
      * @api
      */
     public function findIdentifiersByTag($tag): array
     {
-        $query = new Match('_tags', $tag);
+        $query = new MatchQuery('_tags', $tag);
         $search = new Search($this->elastica);
         $search->search($query);
         $identifiers = [];
         foreach ($search->scroll() as $resultSet) {
             $documents = $resultSet->getDocuments();
             foreach ($documents as $document) {
-                /** @var Document $document Document */
-                $identifiers[] = $document->getId();
+                $documentId = $document->getId();
+                if (empty($documentId)) {
+                    continue;
+                }
+
+                $identifiers[] = $documentId;
             }
         }
 
@@ -292,9 +261,26 @@ class ElasticsearchBackend extends AbstractBackend implements TaggableBackendInt
     }
 
     /**
-     * parses index configuration from yaml file into an array
+     * @param string $entryIdentifier
      *
-     * @return array
+     * @return false|mixed
+     */
+    private function getEntryWithLifeTimeCheck(string $entryIdentifier)
+    {
+        $entry = $this->index->getDocument($entryIdentifier);
+        $data = $entry->getData();
+        $content = false;
+        if (is_array($data) && ($data['_lifetime'] === 0 || $data['_lifetime'] > time())) {
+            $content = $data['content'];
+        }
+
+        return $content;
+    }
+
+    /**
+     * parses index configuration from yaml file into an array.
+     *
+     * @return array<mixed>
      */
     private function parseIndexConfiguration(): array
     {
@@ -309,7 +295,7 @@ class ElasticsearchBackend extends AbstractBackend implements TaggableBackendInt
     }
 
     /**
-     * creates new index with predefined configuration
+     * creates new index with predefined configuration.
      */
     private function createIndex(): void
     {
